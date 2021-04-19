6c7586f0c26f439f2cc7f6b24752ae4b2dbc8bac
Пилипёнок Никита Витальевич <319318@college.local>
2021-04-19 12:16:09 +0300

    first commit

---
+++ Project1/GitHub1/GitHub1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub1
+{
+    class Program
+    {
+        static strana az = new strana(); //Объявляем переменную *strana*
+
+        static void Main(string[] args)
+        {
+            for (int z = 0; z < 10000; z++)
+            { //Создаём меню программы
+                Console.WriteLine(" ------------------Practice №1------------------ ");
+                Console.WriteLine("|              1. Создать список.               |");
+                Console.WriteLine("|              2. Вывести список.               |");
+                Console.WriteLine("|              3. Очистить экран                |");
+                Console.WriteLine("|              4. Выйти из программы.           |");
+                Console.WriteLine(" ----------------------------------------------- ");
+                Console.Write("Введите номер операции которую вы хотите выполнить: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                switch (x)
+                {
+                    case 1: az.add(); break;
+                    case 2: az.print(); break;
+                    case 3: Console.Clear(); break;
+                    case 4: Environment.Exit(0); break;
+                    default: Console.WriteLine("Error! Incorrect input!"); break; //Проверка на некорректный ввод данных, т.е при вводе числа >5 получаем ошибку
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project1/GitHub1/GitHub1/Strana.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub1
+{
+    class strana //Создаём класс страны
+    {
+        static strana[] a = new strana[1000];
+        static int n;
+
+        public string nazvanie { get; set; }
+        public string formapravleniya { get; set; }
+        public float ploshad { get; set; }
+
+        internal void add()
+        {
+            Console.WriteLine();
+            Console.Write("Введите кол-во записей: ");
+            n = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new strana();
+                Console.Write("Название: ");
+                a[i].nazvanie = Console.ReadLine();
+                Console.Write("Форма правления: ");
+                a[i].formapravleniya = Console.ReadLine();
+                Console.Write("Площадь: ");
+                a[i].ploshad = (Convert.ToSingle(Console.ReadLine()));
+                Console.WriteLine();
+            }
+        }
+
+        internal void print() //Вывод данных
+        {
+            Console.WriteLine();
+            Console.WriteLine("Список стран:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Название:" + a[i].nazvanie);
+                Console.WriteLine("Форма правления:" + a[i].formapravleniya);
+                Console.WriteLine("Площадь:" + a[i].ploshad);
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
